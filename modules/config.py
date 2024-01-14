d6203e0c844797dd821278310845766874bb1611
Mateusz Dobrychłop <mateusz.dobrychlop@gmail.com>
2024-01-14 12:42:41 +0100

    Update config.py

--- modules/config.py
+++ modules/config.py
@@ -256,7 +256,7 @@ default_image_number = get_config_item_or_set_default(
 checkpoint_downloads = get_config_item_or_set_default(
     key='checkpoint_downloads',
     default_value={
-        "juggernautXL_version6Rundiffusion.safetensors": "https://huggingface.co/lllyasviel/fav_models/resolve/main/fav/juggernautXL_version6Rundiffusion.safetensors"
+        "juggernautXL_v8Rundiffusion.safetensors": "https://huggingface.co/mdobrychlop/fav/blob/main/juggernautXL_v8Rundiffusion.safetensors"
     },
     validator=lambda x: isinstance(x, dict) and all(isinstance(k, str) and isinstance(v, str) for k, v in x.items())
 )
